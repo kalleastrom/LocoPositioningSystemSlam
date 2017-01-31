4b783b993cc095d08d19a1b225f4967fbd5a0df7
Kalle Åström <kalle@maths.lth.se>
2017-01-31 11:38:08 +0100

    small fixes

--- matlab/read_from_lpsdb.m
+++ matlab/read_from_lpsdb.m
@@ -8,11 +8,23 @@ switch id(1),
     case 1
         tmp = load([systemsettings.rootpath filesep 'data' filesep 'lpsdb' filesep 'data_db_20170119_20170119.mat']);
         data.db = tmp.data_db;
+        load exp_list_20150924B;
+        data.exlist = exlist;
+        data.exp_description = exp_description;
         data.anchorDim = 3; % The dimensionality of the span of the anchor positions
         data.bitcrazeDim = 3; % The dimensionality of the span of the bitcraze positions
     case 2
         tmp = load([systemsettings.rootpath filesep 'data' filesep 'lpsdb' filesep 'data_db_20170119_20170119.mat']);
         data.db = tmp.data_db;
+        for i = 1:length(data.db)
+            data.db{i}=data.db{i}(1:3);
+        end
+        load exp_list_20150924B;
+        data.exlist = exlist;
+        data.exp_description = exp_description;
+        data.db = data.db([1:7]);
+        data.exp_description = data.exp_description(1);
+        data.exlist = data.exlist(1:7);        
         data.anchorDim = 3; % The dimensionality of the span of the anchor positions
         data.bitcrazeDim = 3; % The dimensionality of the span of the bitcraze positions
     otherwise
