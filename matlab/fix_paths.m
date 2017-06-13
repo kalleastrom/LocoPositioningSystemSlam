72b7a3190c32abb23ba4c54b788e6b4a8898db4e
David Gillsjö <davidg@maths.lth.se>
2017-06-13 10:06:29 +0200

    * Fixpaths now also includes multipol

--- matlab/fix_paths.m
+++ matlab/fix_paths.m
@@ -1,8 +1,13 @@
 % fix a few path related things
 
+% Restore path to avoid name clashes with other projects
+restoredefaultpath;
+
 thispath = pwd;
 cd ..
 rootpath = pwd;
+cd ..
+workspace = pwd;
 cd(thispath);
 
 addpath([rootpath filesep 'data']);
@@ -10,5 +15,8 @@ addpath([rootpath filesep 'matlab']);
 addpath([rootpath filesep 'matlab' filesep 'solvers']);
 addpath([rootpath filesep 'matlab' filesep 'simulations']);
 addpath([rootpath filesep 'matlab' filesep 'tools']);
+
+% Also include multipol (assume its in the same folder as this repository)
+addpath([workspace filesep 'multipol'])
 
 systemsettings.rootpath = rootpath;
